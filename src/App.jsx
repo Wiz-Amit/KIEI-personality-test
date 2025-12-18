b1545b170acca5113edcd2fc9660a0b02ad8d4ac
Wiz-Amit <ahazratwt@gmail.com>
2025-12-18 14:57:56 +0530

    refactor: restructure App component layout and enhance accessibility with updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,38 @@ function App() {
   return (
     <Fragment>
       <Navigation />
-      <Container
-        className="container-fluid w-100 h-100 d-flex justify-content-between align-items-center flex-column"
-        style={{
-          minHeight: "88vh",
-        }}
-      >
-        <Switch>
-          <Route strict path="/result">
-            <Result />
-          </Route>
+      <main>
+        <Container
+          className="container-fluid w-100 h-100 d-flex justify-content-between align-items-center flex-column"
+          style={{
+            minHeight: "88vh",
+          }}
+        >
+          <Switch>
+            <Route strict path="/result">
+              <Result />
+            </Route>
 
-          <Route path="/questions/:id">
-            <Slide />
-          </Route>
+            <Route path="/questions/:id">
+              <Slide />
+            </Route>
 
-          <Route strict path="/">
-            <Welcome />
-          </Route>
-        </Switch>
+            <Route strict path="/">
+              <Welcome />
+            </Route>
+          </Switch>
 
-        <p className="text-muted text-center mt-2">
-          Developed by{" "}
-          <a href="https://wizamit.com" target="_blank" rel="noreferrer">
-            WizAmit
-          </a>
-        </p>
-      </Container>
+          <p
+            className="text-muted text-center mt-2"
+            style={{ fontFamily: "monospace" }}
+          >
+            Developed by{" "}
+            <a href="https://wizamit.com" target="_blank" rel="noreferrer">
+              WizAmit
+            </a>
+          </p>
+        </Container>
+      </main>
     </Fragment>
   );
 }
